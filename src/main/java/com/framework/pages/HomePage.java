1cd56202d645394c08d6aa00417be392ec5363a7
Arya <arya28051996@gmail.com>
2026-02-18 12:21:55 +0530

    Improved HomePage code

--- src/main/java/com/framework/pages/HomePage.java
+++ src/main/java/com/framework/pages/HomePage.java
@@ -1,5 +1,6 @@
 package com.framework.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -9,8 +10,33 @@ public class HomePage {
 		this.driver=driver;
 		
 	}
+	
+	//locators
+	By appLogo=By.className("app_logo");
+	By menuBtn=By.id("react-burger-menu-btn");
+	By cartIcon=By.className("shopping_cart_link");
+	
+			
+	//Actions
+	
 	public String getTitle() {
-		return driver.getTitle();
+		String title=driver.getTitle();
+		return title;
+	}
+	public boolean isLogoDisplayed() {
+		boolean logo=driver.findElement(appLogo).isDisplayed();
+		return logo; 
+	}
+	public boolean isMenuBtnDisplayed() {
+		boolean menu=driver.findElement(menuBtn).isDisplayed();
+		return menu;
+	}
+	public boolean isCartIconDisplayed() {
+		boolean cart=driver.findElement(cartIcon).isDisplayed();
+		return cart;
+	}
+	public void onMenuClick() {
+		driver.findElement(menuBtn).click();
 	}
 
 }
